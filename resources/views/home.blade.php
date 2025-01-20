6e6cc06a2d75ad4b916295612017a2b8e46a5498
HanifCahyo <hanifcahyoprasetyo95@gmail.com>
2025-01-20 14:35:49 +0700

    feat:route machine

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,10 +21,35 @@
 
     <script src="leaflet/js/leaflet.textpath.js"></script>
 
+    {{--  Marker Cluster  --}}
     <link rel="stylesheet" href="leaflet/js/MarkerCluster.css" />
     <link rel="stylesheet" href="leaflet/js/MarkerCluster.Default.css" />
 
+
     <script src="leaflet/js/leaflet.markercluster.js"></script>
+
+    {{-- Minimap --}}
+
+    <link rel="stylesheet" href="leaflet/js/Control.MiniMap.min.css" />
+    <script src="leaflet/js/Control.MiniMap.min.js"></script>
+
+    {{-- Leaflet Distance --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/gokertanrisever/leaflet-ruler@master/src/leaflet-ruler.css"
+        integrity="sha384-P9DABSdtEY/XDbEInD3q+PlL+BjqPCXGcF8EkhtKSfSTr/dS5PBKa9+/PMkW2xsY" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/gh/gokertanrisever/leaflet-ruler@master/src/leaflet-ruler.js"
+        integrity="sha384-8SqKZR7V8uOetpjjbcNJHvwuHpb074WS0UXjCLhzfJUqYn3B/uWx1WVv5mwRp1mV" crossorigin="anonymous">
+    </script>
+
+    {{-- Hash --}}
+    <script src="leaflet/js/leaflet-hash.js"></script>
+
+    {{-- Cursor Position --}}
+    <link rel="stylesheet" href="leaflet/js/L.Control.MousePosition.css" />
+    <script src="leaflet/js/L.Control.MousePosition.js"></script>
+
+    {{-- Routing Machine --}}
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+    <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
 
     <!-- Load Leaflet Control Layers from CDN -->
     <style>
@@ -64,8 +89,15 @@
     <br>
 
     <div>
-        <input type="checkbox">Jalan</input>
-        <input type="checkbox">Bidang</input>
+        <input id="jalanCheckbox" type="checkbox" onclick="pilihjalan(this)"> Jalan</input>
+        <input id="lokasiCheckbox" type="checkbox" onclick="pilihlokasi(this)"> Lokasi</input>
+    </div>
+
+    <br>
+    <div>
+        <input id="titik_a" type="text"></input>
+        <input id="titik_b" type="text"></input>
+        <input id="jalan" type="text"></input>
     </div>
 
     <div id="map"></div>
@@ -78,7 +110,9 @@
         accessToken =
         "AAPTxy8BH1VEsoebNVZXo8HurDEMCVzcg2e_v0-cTZW2J9uhkTG4jBjBd_1nmhddasLXJlRV5R9oMn_mWMVsAbdAbCo-kHUobHAUldTiW_L8YNKwjUv-5sl6uEXFXvTPHlU3xWrdgysOCjwzVHH-UDyts0ivA4_hlMu1p7tSZopNm4KF1N3g4CQ6_KLjZDExvlSpdlapJtIIg4nx3YH92LVJF115aqCjfxDSUPRbsu1SQCM.AT1_KSBmHgWL";
     const map = L.map("map", {
-        minZoom: 2
+        minZoom: 2,
+        maxZoom: 18 // Zoom maksimal, sesuaikan dengan kebutuhan
+
     })
 
     // Set the map view to a given center and zoom
@@ -228,6 +262,8 @@
         });
     })
 
+    var lyr_lokasi = L.markerClusterGroup();
+
     // GeoJSON Polygon Dengan Garis Putus"//
     $.getJSON('geojson/map.geojson', function(json) {
         geoLayer = L.geoJson(json, {
@@ -250,9 +286,9 @@
                     html: '<b>' + feature.properties.nama + '</b>',
                     iconSize: [100, 20]
                 });
-                L.marker(layer.getBounds().getCenter(), {
+                var marker = L.marker(layer.getBounds().getCenter(), {
                     icon: iconLabel
-                }).addTo(map)
+                }) //.addTo(map)
 
                 layer.on('click', (e) => {
                     // alert(feature.properties.id)
@@ -275,13 +311,18 @@
                         })
                     });
                 })
+                lyr_lokasi.addLayer(marker);
+                lyr_lokasi.addLayer(layer);
 
-                layer.addTo(map);
+                // layer.addTo(map);
             }
 
         })
     });
 
+    var lyr_jalan = L.markerClusterGroup();
+
+    // GeoJSON Jalan Dengan Garis Putus"//
     $.getJSON('geojson/jalan.geojson', function(json) {
         geoLayer = L.geoJson(json, {
             style: function(feature) {
@@ -311,7 +352,8 @@
                     })
                 })
 
-                layer.addTo(map);
+                lyr_jalan.addLayer(layer);
+                // layer.addTo(map);
             }
 
         })
@@ -364,6 +406,78 @@
         return div;
     };
     legend.addTo(map);
+
+    //Layer Group//
+    function pilihjalan(checkbox) {
+        if (checkbox.checked) {
+            map.addLayer(lyr_jalan);
+        } else {
+            map.removeLayer(lyr_jalan);
+        }
+    }
+
+    function pilihlokasi(checkbox) {
+        if (checkbox.checked) {
+            map.addLayer(lyr_lokasi);
+        } else {
+            map.removeLayer(lyr_lokasi);
+        }
+    }
+
+    // Mini Map//
+    var oSatelite = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+
+
+
+    var miniMap = new L.Control.MiniMap(oSatelite, {
+        toggleDisplay: true,
+        minimized: true,
+    }).addTo(map);
+
+    // Measure Distance//
+
+    var options = {
+        position: 'topleft',
+
+    }
+    L.control.ruler(options).addTo(map);
+
+    // Hash//
+    var hash = new L.Hash(map);
+
+    // Cursor Position//
+    L.control.mousePosition().addTo(map);
+
+    // Routing Machine//
+    var waypoints = [
+        L.latLng(-6.8881001, 110.8505008),
+        L.latLng(-6.7731001, 110.8505008)
+    ];
+
+    var routeControl = L.Routing.control({
+        waypoints: waypoints,
+        routeWhileDragging: true,
+        lineOptions: {
+            styles: [{
+                color: 'blue',
+                opacity: 0.6,
+                weight: 6
+            }]
+        }
+    }).addTo(map);
+
+    routeControl.on('routesfound', function(e) {
+        var distance = e.routes[0].summary.totalDistance;
+        var time = e.routes[0].summary.totalTime;
+        document.getElementById('titik_a').value = e.routes[0].waypoints[0].latLng.lat + ',' + e.routes[0]
+            .waypoints[0].latLng.lng;
+        document.getElementById('titik_b').value = e.routes[0].waypoints[1].latLng.lat + ',' + e.routes[0]
+            .waypoints[1].latLng.lng;
+        document.getElementById('jalan').value = e.routes[0].name;
+    });
 </script>
 
 </html>
